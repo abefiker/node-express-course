b78adea11cdf472a469f954173de4596dcc1c7ca
abfiker <new94abemelek@gmail.com>
2024-04-24 12:59:42 +0300

    mk

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,31 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express();
 app.use('/public',express.static(path.join(__dirname, 'public')));
+const taskRoute = require('./routers/taskRoutes')
+app.use('/api/v1/tasks/',taskRoute)
+
 const port = 3000;
-
-app.get('/api/v1/tasks',(req,res)=>{
-    res.send(200);
-})
-app.post('/api/v1/tasks',(req,res)=>{
-    res.send(200);
-})
-
-app.get('/api/v1/tasks/:id',(req,res)=>{
-    res.send(200);
-})
-
-app.patch('/api/v1/tasks/:id',(req,res)=>{
-    res.send(200);
-})
-app.delete('/api/v1/tasks/:id',(req,res)=>{
-    res.send(200);
-})
-
-
-
-app.get('/hello',(req,res)=>{
-    res.send('Task manager App')
-})
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
--- 03-task-manager/starter/routers/taskRoutes.js
+++ 03-task-manager/starter/routers/taskRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controller/taskController')
-router.get
+router.route('/').get(taskController.getAllTasks).post(taskController.createTask)
+router.route('/:id').get(taskController.getATask).patch(taskController.updateTask).delete(taskController.delteTask)
 
 module.exports = router
