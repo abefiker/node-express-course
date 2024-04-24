545363f5a30290556af5211b09d054638ef40acf
abfiker <new94abemelek@gmail.com>
2024-04-24 13:12:45 +0300

    mk

--- 03-task-manager/starter/controller/taskController.js
+++ 03-task-manager/starter/controller/taskController.js
@@ -1,15 +1,67 @@
-exports.getAllTasks = (req,res) =>{
-    res.send('serving all tasks')
+const Task = require('../model/taskModel')
+exports.getAllTasks = async (req,res) =>{
+    try {
+        const tasks = await Task.find()
+        res.status(200).json(
+            tasks 
+        )
+    } catch (err) {
+        res.status(404).json(
+            msg = err.message,
+        )
+    }
 }
-exports.getATask = (req,res) =>{
-    res.send('serving task')
+exports.getATask =async  (req,res) =>{
+    try {
+        const task = await Task.findById(req.params.id)
+        res.status(200).json(
+            task
+        )
+    } catch (err) {
+        res.status(404).json(
+            msg = err.message,
+        )
+    }
 }
-exports.updateTask = (req,res) =>{
-    res.send('update task')
+exports.updateTask =async  (req,res) =>{
+    try {
+        const task = await Task.findByIdAndUpdate(
+            req.params.id, 
+            { new: true, runValidators: true } 
+        );
+        
+        res.status(200).json(
+            task ,
+        )
+    } catch (err) {
+        res.status(404).json(
+            msg = err.message,
+        )
+    }
 }
-exports.delteTask = (req,res) =>{
-    res.send('deleting  task')
+exports.delteTask = async (req,res) =>{
+    try {
+         await Task.deleteMany()
+        res.status(200).json(
+            msg = 'success',
+        )
+    } catch (err) {
+        res.status(404).json(
+            msg = err.message,
+            status = 'fail',
+        )
+    }
 }
-exports.createTask = (req,res) =>{
-    res.send('creating task')
+exports.createTask = async  (req,res) =>{
+    try {
+        const tasks = await Task.create(req.body)
+        res.status(200).json(
+            tasks ,
+            msg = 'success',
+        )
+    } catch (err) {
+        res.status(404).json(
+            msg = err.message,
+        )
+    }
 }
---
+++ 03-task-manager/starter/model/taskModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const Task = mongoose.model('Task',taskSchema);
+module.exports = Task;
